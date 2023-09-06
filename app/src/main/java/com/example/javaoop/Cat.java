748315874296d6c86d47146557dff54b08185308
Студент 409 лаборатория <student_ait409@pkgod.ru>
2023-09-06 16:27:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/javaoop/Cat.java
@@ -0,0 +1,29 @@
+package com.example.javaoop;
+
+import android.util.Log;
+public class Cat {
+    int age;
+    String name;
+
+    String color;
+    String breed;
+
+    public Cat(){
+
+    }
+
+    public Cat(int age, String name, String color, String breed){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+        this.breed = breed;
+    }
+    public void talk(){
+        Log.i("talk()","Meow! My name is " + name
+                + " and I'm " + age + " years old. Also I'm " + color + " " + breed);
+
+    }
+    public void initFields(){
+
+    }
+}
